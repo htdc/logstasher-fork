7c3dc76ae52295c36f7fc802544387a9c4e6724a
Craig Sheen <craigsheen87@gmail.com>
2025-07-30 09:25:08 +1000

    Apply patch to ensure controller methods are available to add_custom_fields

--- spec/lib/logstasher/instrumentation_spec.rb
+++ spec/lib/logstasher/instrumentation_spec.rb
@@ -25,14 +25,14 @@ describe ActionController::Base do
     it 'adds default fields to payload' do
       expect(LogStasher).to receive(:add_default_fields_to_payload).once
       expect(LogStasher).to receive(:add_default_fields_to_request_context).once
-      subject.send(:process_action, :index)
+      subject.process_action(:index)
     end
 
     it 'creates the request context before processing' do
       LogStasher.request_context[:some_key] = 'value'
       expect(LogStasher).to receive(:clear_request_context).once.and_call_original
       expect do
-        subject.send(:process_action, :index)
+        subject.process_action(:index)
       end.to change { LogStasher.request_context }
     end
 
@@ -41,7 +41,7 @@ describe ActionController::Base do
                                                                         anything).once
       expect(ActiveSupport::Notifications).to receive(:instrument).with('process_action.action_controller',
                                                                         anything).once
-      subject.send(:process_action, :index)
+      subject.process_action(:index)
     end
 
     context 'request context has custom fields defined' do
@@ -56,7 +56,7 @@ describe ActionController::Base do
       end
 
       it 'should retain the value in the request context' do
-        subject.send(:process_action, :index)
+        subject.process_action(:index)
       end
 
       after :each do
@@ -67,6 +67,45 @@ describe ActionController::Base do
         end
         ActionController::Base.class_eval do
           undef logstasher_add_custom_fields_to_request_context
+        end
+      end
+    end
+
+    context "payload has custom fields defined" do
+      before :each do
+        LogStasher.add_custom_fields do |fields|
+          fields[:some_field] = 'value'
+        end
+
+        ActiveSupport::Notifications.subscribe('process_action.action_controller') do |_, _, _, _, payload|
+          @payload = payload
+        end
+      end
+
+      it "should retain the value in the request context" do
+        subject.process_action(:index)
+      end
+
+      context 'exception thrown by controller' do
+        before :each do
+          def subject.index_with_exception(*args)
+            fail 'Exception'
+          end
+        end
+
+        it 'should retain the value in the request context' do
+          expect { subject.process_action(:index_with_exception) }.to raise_exception('Exception')
+        end
+      end
+
+      after :each do
+        expect(@payload[:some_field]).to eq('value')
+
+        ActionController::Metal.class_eval do
+          undef logstasher_add_custom_fields_to_payload
+        end
+        ActionController::Base.class_eval do
+          undef logstasher_add_custom_fields_to_payload
         end
       end
     end
